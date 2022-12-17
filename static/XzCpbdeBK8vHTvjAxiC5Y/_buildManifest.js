53230363528db07f704975ec51e95df56fd5a10c
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-17 14:20:28 +0200

    Updates

--- static/XzCpbdeBK8vHTvjAxiC5Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/259-53011653b4ed73df.js","static/chunks/pages/index-7c1d3de97e808121.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
