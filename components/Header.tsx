43e75e49811deeb4c927977dec02d1496c810d10
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-17 09:45:07 +0200

    edit style

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,18 +22,25 @@ export default function Header({}: Props) {
             transition={{
                 duration: 1.5
             }}
-            className='flex flex-row items-center'>
-            <SocialIcon url="https://www.github.com" 
-            fgColor='gray'
+            className='flex flex-row items-center hover:fgColor-[#ffffff] '
+            >
+            <SocialIcon url="https://github.com/ahmedfarag9" 
+            fgColor='white'
             bgColor='transparent'
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{height: 60, width: 60}}
             />
-            <SocialIcon url="https://www.twitter.com" 
+            {/* <SocialIcon url="https://www.twitter.com" 
             fgColor='gray'
             bgColor='transparent'
-            />
-            <SocialIcon url="https://www.linkedin.com" 
-            fgColor='gray'
+            /> */}
+            <SocialIcon url="https://www.linkedin.com/in/ahmedfarag9/" 
+            fgColor='white'
             bgColor='transparent'
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{height: 60, width: 60}}
             />                        
         </motion.div>
 
@@ -59,14 +66,15 @@ export default function Header({}: Props) {
                 url='#contact'
                 className='cursor-pointer'
                 network='email' 
-                fgColor='gray'
+                fgColor='white'
                 bgColor='transparent'
+                style={{height: 60, width: 60}}
                 />
 
                 <Link href='#contact'>
 
-                    <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-                        Get In Touch    
+                    <p className='uppercase  md:inline-flex text-xs text-gray-400'>
+                       Contact Me    
                     </p>
                 </Link>
             </motion.div>
