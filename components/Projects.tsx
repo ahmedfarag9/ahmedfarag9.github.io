9736d1adfa307011ffd3bd277483402d276acee6
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2022-12-17 09:53:17 +0200

    code refactor

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -23,22 +23,22 @@ function Projects({}: Props) {
 
 
   return (
-<div className="h-screen relative flex overflow-hidden flex-col text-left
-    max-w-full justify-evenly mx-auto items-center z-0 pr-3">
+  <div className="h-screen relative flex overflow-hidden flex-col text-left
+      max-w-full justify-evenly mx-auto items-center z-0 pr-3">
 
-  <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
-      text-2xl'>
-        Projects
-  </h3>
+    <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-400
+        text-2xl'>
+          Projects
+    </h3>
 
-  <div className='container'>
+    <div className='container'>
 
-  <ul className="cards">
+    <ul className="cards">
       {Object.entries(projects).map(([key, project]) =>  (
 
 
         <li key={key} className="card">
-      <div>
+          <div>
             <h3 className="card-title">{project.name}</h3>
               <div className='card-zoom'>
                 <Image
@@ -46,12 +46,12 @@ function Projects({}: Props) {
                 src={project.picture}
                 alt='user profile picture'
                 />
-        </div>
-        <div className="card-content">
+              </div>
+            <div className="card-content">
               <p>{project.description}</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
+            </div>
+          </div>
+          <div className="card-link-wrapper">
             <a
               href={project.link}
               className="card-link"
@@ -59,61 +59,40 @@ function Projects({}: Props) {
               rel="noopener noreferrer">
                 Read More
             </a>
-      </div>
-    </li>
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 3</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar eu eget quam.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 4</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi, quis auctor lorem ornare vel.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 5</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien pulvinar.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 6</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Donec ut tincidunt nisl. Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero finibus tempor.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
+          </div>
+        </li>
 
-  </ul>
-  
-</div>
-</div>
+
+
+
+        // <li key={key} className="card">
+        //   <div>
+        //     <h3 className="card-title">{project.name}</h3>
+        //       <Image
+        //       className='relative rounded-full h-40 w-40 mx-auto object-contain '
+        //       src={project.picture}
+        //       alt='user profile picture'
+        //       />
+        //     <div className="card-content">
+        //       <p>{project.description}</p>
+        //     </div>
+        //   </div>
+        //   <div className="card-link-wrapper">
+        //     <a
+        //       href={project.link}
+        //       className="card-link"
+        //       target="_blank"
+        //       rel="noopener noreferrer">
+        //         Read More
+        //     </a>
+        //   </div>
+        // </li>  
+      ))}
+
+    </ul>
+    
+    </div>
+  </div>
   )
 }
 
