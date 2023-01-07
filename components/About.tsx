14392f281ba2b70c3127df466c89ce1341368e2b
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-01-08 01:47:23 +0200

    add gif image

--- components/About.tsx
+++ components/About.tsx
@@ -11,14 +11,18 @@ function About({}: Props) {
     initial={{ opacity: 0 }}
     transition={{ duration: 1 }}
     whileInView={{opacity: 1}}
-    className=' h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-full
+    className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-full
     px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-400
         text-2xl'>
             About
         </h3>
 
-        {/* <Image
+
+        {/* <img src="https://media2.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif?cid=790b7611750ea31b50f7a50f799f0c0a0252c02163566e18&rid=giphy.gif&ct=g" width="300"/> */}
+
+
+        <Image
         // initial={{
         //     x: -200,
         //     opacity: 0
@@ -28,14 +32,16 @@ function About({}: Props) {
         // }}
         // whileInView={{opacity: 1, x: 0}}
         // viewport={{once: true}}
-        src={profilePic}
+        src='https://media2.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif?cid=790b7611750ea31b50f7a50f799f0c0a0252c02163566e18&rid=giphy.gif&ct=g'
+        height={300}
+        width={300}
         alt='user picture'
         className='mt-24 md:mb-0 flex-shrink-0 w-52 h-52 md:w-[300px] md:h-[300px]
         rounded-full object-cover md:rounded-lg 
-        xl:w-[400px] xl:h-[400px] '
-        /> */}
+        xl:w-[350px] xl:h-[300px] '
+        />
         <div className='space-y-3 px-0 md:px-10 mb-20'>
-            <h4 className='text-xl font-semibold mb-10 text-center'>
+            <h4 className='text-xl font-semibold mb-10 '>
                 Here is a little background
             </h4>
             <p className=' text-l'>
