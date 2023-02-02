357cf0c543128cdc03eff526d3246d6c31df35ed
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-02-02 14:34:58 +0200

    Code refactor

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,19 +6,19 @@ type Props = {}
 export default function ExperienceCard({}: Props) {
     return (
         <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
-        w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929 p-10] hover:opacity-100
+        w-[300px] md:w-[600px] xl:w-[600px] snap-center bg-[#292929 p-10] hover:opacity-100
         opacity-90 cursor-pointer transition-opacity duration-200 overflow-hidden'>
             <motion.img
-            initial={{ 
-                y: -100,
-                opacity: 0 
-            }}
-            transition={{ duration: 1.2 }}
-            whileInView={{
-                opacity: 1,
-            y: 0
-            }}
-            viewport={{ once: true }}
+            // initial={{ 
+            //     y: -100,
+            //     opacity: 0 
+            // }}
+            // transition={{ duration: 1.2 }}
+            // whileInView={{
+            //     opacity: 1,
+            // y: 0
+            // }}
+            // viewport={{ once: true }}
             className='w-32 h-32 rounded-full object-cover object-center' 
                 src="https://st4.depositphotos.com/21230196/23977/v/450/depositphotos_239776244-stock-illustration-specialist-icon-vector-white-background.jpg"
                 alt="" 
