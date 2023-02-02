34f7c9f5379f80206833b87416c3441cb441dd2a
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-02-02 14:34:20 +0200

    Add elearning project info

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import robotPic from '../public/robot.jpg'
 import portfolioPic from '../public/portfolio.png'
 import cicdPic from '../public/cicd.png'
 import uniSearchPic from '../public/uniSearch.png'
+import elearningWebsitePic from '../public/e-learning-website.png'
 
 
 
@@ -14,11 +15,12 @@ type Props = {}
 function Projects({}: Props) {
 
   const projects = {
-    1: {name:"Modern Portfolio", picture: portfolioPic, description:"Modern Portfolio Built with REACT.JS & NEXT.JS Using (JavaScript, TypeScript, Framer Motion, Tailwind CSS)", link:"https://github.com/ahmedfarag9/modern-portfolio-reactjs-nextjs"},
-    2: {name:"CI-CD Pipeline", picture: cicdPic, description:"A Multi-branch CI-CD Pipeline Using Jenkins, Docker & AWS To Deploy Odoo ERP instance & a simple Java Maven app.", link:"https://github.com/ahmedfarag9/CI-CD-Pipeline-Jenkins-Odoo-Project"},
-    3: {name:"Humanoid Robot", picture: robotPic, description:"A Humanoid Robot Built From Scratch (Graduation Project).", link:"https://github.com/ahmedfarag9/humaniod-robot-graduation-project"},
-    4: {name:"Uni-Search Egypt", picture: uniSearchPic, description:"A React-Native mobile app to search among hundreds of Egyptian Universities. (published on Google Play)", link:"https://github.com/ahmedfarag9/UniSearch-Egypt"},
-   }
+    1: {name:"Modern Portfolio", picture: portfolioPic, description:"Modern Portfolio Built with REACT.JS & NEXT.JS Using (JavaScript, TypeScript, Framer Motion, Tailwind CSS)", link:"https://github.com/ahmedfarag9/modern-portfolio-reactjs-nextjs", preview:''},
+    2: {name:"CI-CD Pipeline", picture: cicdPic, description:"A Multi-branch CI-CD Pipeline Using Jenkins, Docker & AWS To Deploy Odoo ERP instance & a simple Java Maven app.", link:"https://github.com/ahmedfarag9/CI-CD-Pipeline-Jenkins-Odoo-Project", preview: ''},
+    3: {name:"E-learning Website", picture: elearningWebsitePic, description:"An E-learning Website Built with pure HTML, JAVASCRIPT & CSS", link:"https://starlit-llama-47c3ea.netlify.app/", preview:"https://starlit-llama-47c3ea.netlify.app/"},
+    4: {name:"Humanoid Robot", picture: robotPic, description:"A Humanoid Robot Built From Scratch (Graduation Project).", link:"https://github.com/ahmedfarag9/humaniod-robot-graduation-project", preview: ''},
+    5: {name:"Uni-Search Egypt", picture: uniSearchPic, description:"A React-Native mobile app to search among hundreds of Egyptian Universities. (published on Google Play)", link:"https://github.com/ahmedfarag9/UniSearch-Egypt", preview: ''},
+  }
 
 
 
@@ -42,7 +44,7 @@ function Projects({}: Props) {
             <h3 className="card-title">{project.name}</h3>
               <div className='card-zoom'>
                 <Image
-                className='card-zoom-image '
+                className='card-zoom-project-image '
                 src={project.picture}
                 alt='user profile picture'
                 />
@@ -54,7 +56,7 @@ function Projects({}: Props) {
           <div className="card-link-wrapper">
             <a
               href={project.link}
-              className="card-link"
+              className="card-link mb-5"
               target="_blank"
               rel="noopener noreferrer">
                 Read More
