2a4f5c93e05f801ea5299de60f9c1e0126b9e899
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-02-25 22:38:44 +0200

    Add mlops project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,9 +7,9 @@ import portfolioPic from '../public/portfolio.png'
 import cicdPic from '../public/cicd.png'
 import uniSearchPic from '../public/uniSearch.png'
 import elearningWebsitePic from '../public/e-learning-website.png'
-import airbyteAirflow from '../public/airbyte_airflow.png'
-import redis from '../public/redis.png'
-
+import airbyteAirflowPic from '../public/airbyte_airflow.png'
+import redisPic from '../public/redis.png'
+import mlopsPic from '../public/mlops.png'
 
 
 type Props = {}
@@ -18,12 +18,13 @@ function Projects({}: Props) {
 
   const projects = {
     1: {name:"CI-CD Pipeline", picture: cicdPic, description:"A Multi-branch CI-CD Pipeline Using Jenkins, Docker & AWS To Deploy Odoo ERP instance & a simple Java Maven app.", link:"https://github.com/ahmedfarag9/CI-CD-Pipeline-Jenkins-Odoo-Project", preview: ''},
-    2: {name:"Airbyte Airflow Intergration", picture: airbyteAirflow, description:"An Airbyte/Airflow Api integration - Airbyte handles the Api connection & Airflow does the orchestration.", link:"https://github.com/ahmedfarag9/airbyte-airflow-api-intergration", preview: ''},
-    3: {name:"Redis Setup on AWS", picture: redis, description:"Guide to Setup Redis Development Stack on AWS EC2 instance", link:"https://github.com/ahmedfarag9/redis-setup-aws-ec2", preview: ''},
-    4: {name:"Modern Portfolio", picture: portfolioPic, description:"The one you're reading now. Built with REACT.JS & NEXT.JS Using (JavaScript, TypeScript, Framer Motion, Tailwind CSS)", link:"https://github.com/ahmedfarag9/modern-portfolio-reactjs-nextjs", preview:''},
-    5: {name:"E-learning Website", picture: elearningWebsitePic, description:"An E-learning Website Built with pure HTML, JAVASCRIPT & CSS", link:"https://github.com/ahmedfarag9/elearning-website", preview:"https://starlit-llama-47c3ea.netlify.app/"},
-    6: {name:"Humanoid Robot", picture: robotPic, description:"A Humanoid Robot Built From Scratch (Graduation Project).", link:"https://github.com/ahmedfarag9/humaniod-robot-graduation-project", preview: ''},
-    7: {name:"Uni-Search Egypt", picture: uniSearchPic, description:"A React-Native mobile app to search among hundreds of Egyptian Universities. (published on Google Play)", link:"https://github.com/ahmedfarag9/UniSearch-Egypt", preview: ''},
+    2: {name:"MLOPS CICD Housepricing", picture: mlopsPic, description:"A MLOPS CI-CD imlementaion of a Linear Regression Machine Learning Model that predicts house pricing based on various features.", link:"https://github.com/ahmedfarag9/mlops-cicd-bostonhousepricing", preview: ''},
+    3: {name:"Airbyte Airflow Intergration", picture: airbyteAirflowPic, description:"An Airbyte/Airflow Api integration - Airbyte handles the Api connection & Airflow does the orchestration.", link:"https://github.com/ahmedfarag9/airbyte-airflow-api-intergration", preview: ''},
+    4: {name:"Redis Setup on AWS", picture: redisPic, description:"Guide to Setup Redis Development Stack on AWS EC2 instance", link:"https://github.com/ahmedfarag9/redis-setup-aws-ec2", preview: ''},
+    5: {name:"Modern Portfolio", picture: portfolioPic, description:"The one you're reading now. Built with REACT.JS & NEXT.JS Using (JavaScript, TypeScript, Framer Motion, Tailwind CSS)", link:"https://github.com/ahmedfarag9/modern-portfolio-reactjs-nextjs", preview:''},
+    6: {name:"E-learning Website", picture: elearningWebsitePic, description:"An E-learning Website Built with pure HTML, JAVASCRIPT & CSS", link:"https://github.com/ahmedfarag9/elearning-website", preview:"https://starlit-llama-47c3ea.netlify.app/"},
+    7: {name:"Humanoid Robot", picture: robotPic, description:"A Humanoid Robot Built From Scratch (Graduation Project).", link:"https://github.com/ahmedfarag9/humaniod-robot-graduation-project", preview: ''},
+    8: {name:"Uni-Search Egypt", picture: uniSearchPic, description:"A React-Native mobile app to search among hundreds of Egyptian Universities. (published on Google Play)", link:"https://github.com/ahmedfarag9/UniSearch-Egypt", preview: ''},
   }
 
 
@@ -37,7 +38,7 @@ function Projects({}: Props) {
           Projects
     </h3>
 
-    <div className='container'>
+    <div className='container mt-24 mb-5'>
 
     <ul className="cards">
       {Object.entries(projects).map(([key, project]) =>  (
